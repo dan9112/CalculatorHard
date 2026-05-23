7dc39b07000ecda79dc1a7c2b35c7cf62efe087b
danil <danbes9112@gmail.com>
2026-05-23 21:19:50 +0300

    Fixed small tab size behaviour

--- composeApp/src/webMain/kotlin/org/example/calculator_hard/main.kt
+++ composeApp/src/webMain/kotlin/org/example/calculator_hard/main.kt
@@ -1,26 +1,9 @@
 package org.example.calculator_hard
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeViewport
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
@@ -28,15 +11,16 @@ import com.arkivanov.essenty.lifecycle.doOnDestroy
 import com.arkivanov.essenty.lifecycle.resume
 import com.arkivanov.essenty.lifecycle.stop
 import kotlinx.browser.document
+import kotlinx.browser.window
 import org.example.calculator_hard.presentation.MIN_SCREEN_HORIZONTAL_HEIGHT
 import org.example.calculator_hard.presentation.MIN_SCREEN_HORIZONTAL_WIDTH
 import org.example.calculator_hard.presentation.MIN_SCREEN_VERTICAL_HEIGHT
 import org.example.calculator_hard.presentation.MIN_SCREEN_VERTICAL_WIDTH
-import org.example.calculator_hard.presentation.PADDINGS
 import org.example.calculator_hard.presentation.RootScreen
 import org.example.calculator_hard.presentation.createRootComponent
 import org.example.calculator_hard.presentation.rememberOrientation
 import org.example.calculator_hard.shared.startKoin
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.events.Event
 import kotlin.js.ExperimentalWasmJsInterop
 import kotlin.js.js
@@ -50,72 +34,30 @@ fun main() {
     )
     lifecycle.attachToDocument()
 
-    ComposeViewport {
-        val density = LocalDensity.current
-        val orientation = rememberOrientation()
-
-        // Вычисляем минимум в пикселях
-        val (minW, minH) = remember(orientation, density) {
-            val w = when (orientation) {
-                Orientation.Horizontal -> MIN_SCREEN_HORIZONTAL_WIDTH.dp
-                Orientation.Vertical -> MIN_SCREEN_VERTICAL_WIDTH.dp
-            }
-            val h = when (orientation) {
-                Orientation.Horizontal -> MIN_SCREEN_HORIZONTAL_HEIGHT.dp
-                Orientation.Vertical -> MIN_SCREEN_VERTICAL_HEIGHT.dp
-            }
-            w to h
+    (document.documentElement as? HTMLElement)?.style?.run {
+        if (window.innerWidth >= window.innerHeight) {
+            minWidth = "${MIN_SCREEN_HORIZONTAL_WIDTH * window.devicePixelRatio}px"
+            minHeight = "${MIN_SCREEN_HORIZONTAL_HEIGHT * window.devicePixelRatio}px"
+        } else {
+            minWidth = "${MIN_SCREEN_VERTICAL_WIDTH * window.devicePixelRatio}px"
+            minHeight = "${MIN_SCREEN_VERTICAL_HEIGHT * window.devicePixelRatio}px"
         }
 
-        val verticalScrollState = rememberScrollState()
-        val horizontalScrollState = rememberScrollState()
+        overflowX = "auto"
+        overflowY = "auto"
+    }
 
-        // ✅ RootScreen остаётся ЧИСТЫМ
-        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-            val maxWidth = maxWidth
-            val maxHeight = maxHeight
+    ComposeViewport {
+        val orientation = rememberOrientation()
 
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .horizontalScroll(state = horizontalScrollState)
-                    .verticalScroll(state = verticalScrollState)
-            ) {
-                RootScreen(
-                    modifier = Modifier
-                        .width(maxWidth.coerceAtLeast(minW))
-                        .height(maxHeight.coerceAtLeast(minH)),
-                    rootComponent = rootComponent,
-                    orientation = orientation
-                )
-            }
-
-            if (maxWidth < minW) {
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.BottomStart)
-                        .fillMaxWidth()
-                        .height(PADDINGS.dp)
-                        .background(color = Color.DarkGray)
-                        .padding(end = if (maxHeight < minH) PADDINGS.dp else 0.dp)
-                        .background(color = Color.Magenta, shape = RoundedCornerShape(PADDINGS.dp))
-                ) {
-                    // todo: add thumb later!
-                }
-            }
-            if (maxHeight < minH) {
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .fillMaxHeight()
-                        .width(PADDINGS.dp)
-                        .background(color = Color.DarkGray)
-                        .padding(bottom = if (maxWidth < minW) PADDINGS.dp else 0.dp)
-                        .background(color = Color.Magenta, shape = RoundedCornerShape(PADDINGS.dp))
-                ) {
-                    // todo: add thumb later!
-                }
-            }
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            RootScreen(
+                modifier = Modifier.fillMaxSize(),
+                rootComponent = rootComponent,
+                orientation = orientation
+            )
         }
     }
 }
