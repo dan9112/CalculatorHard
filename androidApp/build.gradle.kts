d2fcb2c3bcd353596743f6c85974c3fbaa6d0a85
Danil Bessonov <danbes1997@gmail.com>
2026-06-24 08:40:40 +0300

    build: added SplashScreen API dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -78,4 +78,6 @@ dependencies {
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
     implementation(libs.compose.material3)
+
+    implementation(libs.androidx.core.splashscreen)
 }
