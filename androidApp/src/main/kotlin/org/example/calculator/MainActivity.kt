a6ae905f42c645e9c5181564727dffc084531180
Danil Bessonov <danbes1997@gmail.com>
2026-06-24 11:58:15 +0300

    feat: added Android Splash Screen

--- androidApp/src/main/kotlin/org/example/calculator/MainActivity.kt
+++ androidApp/src/main/kotlin/org/example/calculator/MainActivity.kt
@@ -4,11 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
@@ -19,31 +14,56 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arkivanov.decompose.defaultComponentContext
 import org.example.calculator.presentation.PADDINGS
-import org.example.calculator.presentation.RootComponent
 import org.example.calculator.presentation.RootScreen
+import org.example.calculator.presentation.ThemeAttributeValue.Idle
 import org.example.calculator.presentation.ThemeAttributeValue.Value
 import org.example.calculator.presentation.VerticalPadding
 import org.example.calculator.presentation.ui.theme.AppTheme
 import org.example.calculator.presentation.ui.theme.ContrastLevel
 import org.example.calculator.presentation.ui.theme.isSystemDark
 import org.example.calculator.shared.createRootComponent
+import java.lang.System.currentTimeMillis
 
 class MainActivity : AppCompatActivity() {
+    // todo: figure out why animation ignores its duration time!
+    private var startTime = -1L
+
+    override fun onSaveInstanceState(outState: Bundle) = super.onSaveInstanceState(
+        outState.apply {
+            putLong(START_TIME_FLAG_KEY, startTime)
+        },
+    )
+
+    private companion object {
+        const val START_TIME_FLAG_KEY = "start"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen().apply {
+            if (startTime == -1L) startTime = currentTimeMillis()
+        }
+        savedInstanceState?.let {
+            startTime = it.getLong(START_TIME_FLAG_KEY, -1)
+        }
+
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         val rootComponent = createRootComponent(componentContext = defaultComponentContext())
+        splashScreen.setKeepOnScreenCondition {
+            rootComponent
+                .settingsComponent
+                .run {
+                    theme.value == Idle || contrastLevel.value == Idle || (dynamic != null && dynamic!!.value == Idle) ||
+                        startTime == -1L || currentTimeMillis() - startTime < 3200L
+                }
+        }
 
         setContent {
             val theme by rootComponent.settingsComponent.theme.collectAsState()
@@ -52,10 +72,6 @@ class MainActivity : AppCompatActivity() {
             val currentContrast = contrast
             val dynamic by rootComponent.settingsComponent.dynamic!!.collectAsState()
             val currentDynamic = dynamic
-
-            val splashScreenFinished by rootComponent.splashScreenFinished.collectAsState()
-
-            var isAnimationFinished by remember { mutableStateOf(value = false) }
 
             val layoutDirection = LocalLayoutDirection.current
 
@@ -73,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                 Scaffold(containerColor = MaterialTheme.colorScheme.primaryContainer) { innerPadding ->
                     val padding = innerPadding + PaddingValues(all = PADDINGS.dp)
 
-                    if (currentTheme is Value && currentContrast is Value && currentDynamic is Value && isAnimationFinished) {
+                    if (currentTheme is Value && currentContrast is Value && currentDynamic is Value) {
                         RootScreen(
                             component = rootComponent.calculationComponent,
                             modifier = Modifier
@@ -92,43 +108,6 @@ class MainActivity : AppCompatActivity() {
                             updateContrast = rootComponent.settingsComponent::updateContrastLevel,
                             dynamic = currentDynamic.value,
                             updateDynamic = rootComponent.settingsComponent::updateDynamic,
-                        )
-                    } else {
-                        // todo: replace with SplashScreen API later!
-                        val currentProgress by animateFloatAsState(
-                            targetValue = splashScreenFinished,
-                            animationSpec = tween(
-                                durationMillis = RootComponent.step.inWholeMilliseconds.toInt(),
-                                easing = LinearEasing,
-                            ),
-                        ) { if (it >= 1f) isAnimationFinished = true }
-
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .background(
-                                    brush = when (currentProgress) {
-                                        0f -> Brush.linearGradient(
-                                            listOf(
-                                                Color.Green,
-                                                Color.Blue,
-                                            ),
-                                        )
-
-                                        1f -> Brush.linearGradient(
-                                            listOf(
-                                                Color.Red,
-                                                Color.Green,
-                                            ),
-                                        )
-
-                                        else -> Brush.linearGradient(
-                                            0f to Color.Red,
-                                            currentProgress to Color.Green,
-                                            1f to Color.Blue,
-                                        )
-                                    },
-                                ),
                         )
                     }
                 }
